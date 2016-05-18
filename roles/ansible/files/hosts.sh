ff8fdeae4b2afb19ce66c582111a83977ed2b2a3
meriem-mkhinini <mkhinini.meriem@gmail.com>
2016-05-18 17:04:40 +0200

    ubigate update

--- roles/ansible/files/hosts.sh
+++ roles/ansible/files/hosts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/pi" >> /etc/cron.d/oh-my-zsh-update
-echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/cron.d/ansible-pull
+#echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/cron.d/ansible-pull
 echo "127.0.0.1" >> /etc/ansible/hosts
 rm /home/pi/hosts.sh
